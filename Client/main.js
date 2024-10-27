8e392d1419acb058047f2eccb104d4482548694b
Web4 <167559384+Web4application@users.noreply.github.com>
2024-10-27 08:55:35 -0400

    Update main.js

--- Client/main.js
+++ Client/main.js
@@ -1,5 +1,19 @@
-import './style.css'
-import web4Logo from '/web4.png.jpg'
+// Import the SDK
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+import "./style.css";
+import web4logo from '/web4.png.jpg';
+
+// Instantiate the SDK
+const discordSdk = new DiscordSDK(import.meta.env.1169709827145089064);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is ready");
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
