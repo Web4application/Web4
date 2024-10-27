cb8e93dd6d8476cb38fff3f693bef00dec4f2ee9
Web4 <167559384+Web4application@users.noreply.github.com>
2024-10-27 07:13:14 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import rocketLogo from '/web4.png'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <img src="${web4logo}" class="logo" alt="web4" />
+    <h1>Hello, World!</h1>
+  </div>
+`;
