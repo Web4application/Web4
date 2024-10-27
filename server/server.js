91240fd021c4c142ad32fbb2f061b4434cb15e52
Web4 <167559384+Web4application@users.noreply.github.com>
2024-10-27 07:56:59 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,14 +12,14 @@ app.use(express.json());
 app.post("/api/token", async (req, res) => {
   
   // Exchange the code for an access_token
-  const response = await fetch(`https://discord.com/api/oauth2/token`, {
+  const response = await fetch(`https://discord.com/oauth2/authorize?client_id=1169709827145089064&permissions=1161638869747712&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.com%2Foauth2%2Fautho`, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     },
     body: new URLSearchParams({
-      client_id: process.env.VITE_DISCORD_CLIENT_ID,
-      client_secret: process.env.DISCORD_CLIENT_SECRET,
+      client_id: process.env.1169709827145089064,
+      client_secret: process.env.4ilpUWeLYOEAS_uqpkQ_k6_rLSbhZlxS,
       grant_type: "authorization_code",
       code: req.body.code,
     }),
