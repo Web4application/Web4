82dc185dadde54beb00ddc18d74c2796023e6f16
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-08 20:57:05 +0100

    Create Code.sh

---
+++ Code.sh
@@ -0,0 +1,5 @@
+/bin/web4.exe          # Web4 core
+/web4lib/              # User-specific data
+    user123/
+        weblib/        # User's personal storage
+/modelstore/           # Global AI models for all users
