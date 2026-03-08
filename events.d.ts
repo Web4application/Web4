d61c62fb3235ebf88c25489919495dffa4640845
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-09 00:08:41 +0100

    Create events.d.ts

---
+++ events.d.ts
@@ -0,0 +1,65 @@
+import 'vite/types/customEvent.d.ts'
+// client side
+if (import.meta.hot) {
+  import.meta.hot.on('my:greetings', (data) => {
+    console.log(data.msg) // hello
+  })
+}
+interface HmrContext {
+  file: string
+  timestamp: number
+  modules: Array<ModuleNode>
+  read: () => string | Promise<string>
+  server: ViteDevServer
+}
+declare module 'vite/types/customEvent.d.ts' {
+  interface CustomEventMap {
+    'custom:foo': { msg: string }
+    // 'event-key': payload
+  }
+}
+type CustomFooPayload = InferCustomEventPayload<'custom:foo'>
+import.meta.hot?.on('custom:foo', (payload) => {
+  // The type of payload will be { msg: string }
+})
+import.meta.hot?.on('unknown:event', (payload) => {
+  // The type of payload will be any
+})
+// client side
+if (import.meta.hot) {
+  import.meta.hot.send('my:from-client', { msg: 'Hey!' })
+}
+type IndexHtmlTransformHook = (
+  html: string,
+  ctx: {
+    path: string
+    filename: string
+    server?: ViteDevServer
+    bundle?: import('rollup').OutputBundle
+    chunk?: import('rollup').OutputChunk
+  },
+) =>
+  | IndexHtmlTransformResult
+  | void
+  | Promise<IndexHtmlTransformResult | void>
+
+type IndexHtmlTransformResult =
+  | string
+  | HtmlTagDescriptor[]
+  | {
+      html: string
+      tags: HtmlTagDescriptor[]
+    }
+
+interface HtmlTagDescriptor {
+  tag: string
+  /**
+   * attribute values will be escaped automatically if needed
+   */
+  attrs?: Record<string, string | boolean>
+  children?: string | HtmlTagDescriptor[]
+  /**
+   * default: 'head-prepend'
+   */
+  injectTo?: 'head' | 'body' | 'head-prepend' | 'body-prepend'
+}
