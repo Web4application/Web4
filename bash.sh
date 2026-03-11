80b63be034029144f91a83fddb1ed02a9001cdc7
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-11 01:45:04 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,7 @@
+npx web4-deploy ./dist --accountId web4.near
+npx web4-deploy --nearfs ./path/to/files --accountId your-account.near
+npx web4-deploy . --accountId web4.near
+mkdir my-web4-site
+cd my-web4
+echo "<h1>Hello Web4!</h1>" > index.html
+npm install -g web4-deploy
