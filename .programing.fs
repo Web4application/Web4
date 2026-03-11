eee283f141d085d31719757cd164d5945a3b53ae
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-11 01:32:51 +0100

    Update .programing.fs

--- .programing.fs
+++ .programing.fs
@@ -10,10 +10,42 @@ open SWX
 
 [<EntryPoint>]
 let main argv =
-    printfn "--- RFCS System ---"
+    printfn "--- RFCS Starter Project ---\n"
+
+    // RFCs
     RFCSystem.printRFCs()
+    printfn ""
+
+    // Students
     StudentSystem.printStudents StudentSystem.students
-    printfn "Logic Engine Test:"
+    printfn ""
+
+    // Math
+    printfn "5 + 3 = %d" (MathSystem.add 5 3)
+    printfn "5 ^ 3 = %d" (MathSystem.power 5 3)
+    printfn ""
+
+    // Logic engine
     LogicEngine.addFact "parent" ["John"; "Mary"]
-    printfn "%b" (LogicEngine.queryFact "parent" ["John"; "Mary"])
+    printfn "Is John parent of Mary? %b" (LogicEngine.queryFact "parent" ["John"; "Mary"])
+    printfn ""
+
+    // PSX
+    printfn "Super Add 1+2+3 = %d" (PSX.superAdd 1 2 3)
+    printfn "Factorial 5 = %d" (PSX.factorial 5)
+    printfn ""
+
+    // BEX
+    let conn = { BEX.Server = "localhost"; Database = "RFCS_DB" }
+    BEX.connect conn
+    printfn ""
+
+    // ASX
+    let data = [10;20;30;40]
+    printfn "Average = %f" (ASX.average data)
+    printfn "Sum = %d" (ASX.sum data)
+    printfn ""
+
+    // SWX
+    SWX.renderMarkdown "# Hello from SWX\nThis is Markdown rendered!"
     0
